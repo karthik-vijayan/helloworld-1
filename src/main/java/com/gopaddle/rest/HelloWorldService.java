1e296f766b4e8a9e61ddf8f714d50169a94e56ea
karthik <karthikvijayan03@gmail.com>
2017-12-08 18:26:48 +0530

    hello world service

---
+++ src/main/java/com/gopaddle/rest/HelloWorldService.java
@@ -0,0 +1,22 @@
+package com.gopaddle.rest;
+ 
+import com.sun.jersey.spi.dispatch.RequestDispatcher;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class HelloWorldService {
+ 
+	@GET
+	public Response getMsg() {
+ 
+		String output = "Hello World" ;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
